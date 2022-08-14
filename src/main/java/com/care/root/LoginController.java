6cc63e758ddcecf34dd7b4d78f8bd8c7a5e3670a
AlanKangD <kno12350@gmail.com>
2022-08-14 16:33:53 +0900

    데이터베이스 admin 아이디 생성완료

--- src/main/java/com/care/root/LoginController.java
+++ src/main/java/com/care/root/LoginController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+	public static String adminid = "hoon";
+	public static String adminpw = "1234";
 
 	@RequestMapping("loginbtn")
 	public String login(@RequestParam String userid, @RequestParam String userpw) { 
