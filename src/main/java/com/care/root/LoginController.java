f0334993e5217b2837f0b35e91cc44cbf6ebbfdf
AlanKangD <kno12350@gmail.com>
2022-08-14 16:55:59 +0900

    회원 테이블 생성완료

--- src/main/java/com/care/root/LoginController.java
+++ src/main/java/com/care/root/LoginController.java
@@ -8,10 +8,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class LoginController {
 	public static String adminid = "hoon";
 	public static String adminpw = "1234";
+	public static String[] idDataBase = new String[8];
+	public static String[] pwDataBase = new String[8];
 
 	@RequestMapping("loginbtn")
 	public String login(@RequestParam String userid, @RequestParam String userpw) { 
-		
+		for(int i=0; i<idDataBase.length; i++) {
+			idDataBase[i] = "id" + i;
+			pwDataBase[i] = "pw" + i;
+			System.out.println(idDataBase[i]);
+			System.out.println(pwDataBase[i]);
+		} //아이디 비빌 번호 설정 완료 
+		System.out.println("아이디 정보 : " + idDataBase.toString());
+		System.out.println("비밀번호 정보 : " + pwDataBase.toString());
 		
 		if(userid.equals(adminid)) {
 			if(userpw.equals(adminpw)) {
