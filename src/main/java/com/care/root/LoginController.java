75abe4b510044283d8da5e19402b9e4bca11a195
koseonghun <rhtjdgns77@naver.com>
2022-08-15 14:52:10 +0900

    로그인 처리

--- src/main/java/com/care/root/LoginController.java
+++ src/main/java/com/care/root/LoginController.java
@@ -12,30 +12,41 @@ public class LoginController {
 	public static String[] pwDataBase = new String[8];
 
 	@RequestMapping("loginbtn")
-	public String login(@RequestParam String userid, @RequestParam String userpw) { 
-		for(int i=0; i<idDataBase.length; i++) {
+	public String login(@RequestParam String userid, @RequestParam String userpw) {
+
+		for (int i = 0; i < idDataBase.length; i++) {
 			idDataBase[i] = "id" + i;
 			pwDataBase[i] = "pw" + i;
 			System.out.println(idDataBase[i]);
 			System.out.println(pwDataBase[i]);
-		} //아이디 비빌 번호 설정 완료 
+		} // 아이디 비밀 번호 설정 완료
 		System.out.println("아이디 정보 : " + idDataBase.toString());
 		System.out.println("비밀번호 정보 : " + pwDataBase.toString());
-		
-		if(userid.equals(adminid)) {
-			if(userpw.equals(adminpw)) {
-				return "successlogin";
-			}else {
-				System.out.println("비밀번호가 틀렸습니다.");
+
+		for (int y = 0; y <= 8; y++) {
+			if (userid.equals(idDataBase[y])) {
+				if (userpw.equals(pwDataBase[y])) {
+					return "successlogin";
+				}
+				return "check";
+			} else if (userid.equals(adminid)) {
+				if (userpw.equals(adminpw)) {
+					return "adminpage";
+				}
 			}
-			
-		}else {
-				System.out.println("아이디가 틀렸습니다.");
 		}
-		
-		return "faillogin";
-				
-		
+		return "faillogin";	
 	}
 }
-
+// if(userid.equals(adminid)) {
+//			if(userpw.equals(adminpw)) {
+//				return "successlogin";
+//			}else {
+//				System.out.println("비밀번호가 틀렸습니다.");
+//			}
+//			
+//		}else {
+//				System.out.println("아이디가 틀렸습니다.");
+//		}
+//		
+//		return "faillogin";
